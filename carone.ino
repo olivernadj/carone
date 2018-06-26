5a005542345ef8470a2d318775a248219b7d0396
oliver nadj <oliver.nadj@lazada.com>
2018-06-26 07:26:27 +0700

    Added hall effect sensor and counter. Removed the controller logic keep only core fetures

--- carone.ino
+++ carone.ino
@@ -1,67 +1,115 @@
 #include <ParaSerialWrite.h> // ParaSerialWrite library for parallel UART transmission
 #include <pt.h>              // protothread library
 
-const int ledPin              = 13; //debug purpose
-const int inputSelector       = 7; // hijack gyro sensor communication 
-const int leftForwardButton   = 6; // user control panel buttons
-const int leftBackwardButton  = 5;
-const int rightForwardButton  = 4;
-const int rightBackwardButton = 3;
+#define _DEBUG 1
+#define _SERIAL 9600 // bandwidth or 0 means turned off
+
+const int debugPin            = 6;
+const int inputSelector       = 7; // hijack gyro sensor communication
+const int hallLeftYellowPin   = 2;
+const int hallLeftBluePin     = 4;
+const int hallRightYellowPin   = 3;
+const int hallRightBluePin     = 5;
 
 ParaSerialWrite mySerial(2); // register 2 lower ports of PORTB for parallel UART transmission
 signed int leftSpeed = 0; // left wheel speed
 signed int rightSpeed = 0; // right wheel speed
-const int maxSpeed = 300; // absolute maximum speed
-const int maxManeuveringSpeed = 100; // maximum maneuvering speed
-const int acceleration = 50; // x unit acceleration / second^2
+bool leftClockwise = false;
+bool rightClockwise = false;
 
-enum controlOptions { bySerial, byButton }; // control by serial input or pull up buttons
-int control = byButton; // control set to button
+unsigned long lastLeftIncrCalculated = 0;
+unsigned int leftIncrements = 0;
+unsigned long leftCyclePerSec = 0;
+
+
+const int maxSpeed = 350; // absolute maximum speed
+const int maxManeuveringSpeed = 150; // maximum maneuvering speed
+const int acceleration = 100; // x unit acceleration / second^2
+const int framesInMessage = 10; // vary by models. how many frames transfer before ~500us pause
+
+enum controlOptions { bySerial, byI2C }; // control by serial input or pull up buttons
+int control = bySerial; // control set to button
 char c = 'x'; //last received input by serial. x = neutral
 
-static struct pt ptTx, ptLC; // each protothread needs one of these
+static struct pt ptTx, ptLC, pIncr, ptDebug; // each protothread needs one of these
+
+
+//
+// Debugging
+//
+// This function generates a brief pulse
+// for debugging or measuring on an oscilloscope.
+void debugPulse(int pin, uint16_t count)
+{
+  bool pinStatus = digitalRead(pin);
+  while (count--)
+  {
+    digitalWrite(pin, 1 ^ pinStatus);
+    digitalWrite(pin, pinStatus);
+  }
+}
 
 /***
  * Transmits imitated UART signal, just like gyro sensor.
- * Uses ParaSerialWrite library what can do multiple UART transmission with 
+ * Uses ParaSerialWrite library what can do multiple UART transmission with
  * same frame size and baud-rate
  */
 void writeCurrentSpeed() {
   if (digitalRead(inputSelector) == LOW) {
     mySerial.write(0, 0);
   } else {
+    static unsigned int currentFrame = 1;
     //0xC00 2 stop bits and <<1 for the start
-    uint16_t startSignal = 0xFC00 | (256 << 1); 
-    uint16_t endSignal = 0xFC00 | (85 << 1);    
-    mySerial.write(startSignal, startSignal);
-    _delay_loop_2(150); // delays between frames
-    mySerial.write(
-      0xFC00 | ((leftSpeed & 0xFF) << 1),
-      0xFC00 | ((-rightSpeed & 0xFF) << 1)
-    );
-    _delay_loop_2(150);
-    mySerial.write(
-      0xFC00 | (((leftSpeed >> 8) & 0xFF) << 1),
-      0xFC00 | (((-rightSpeed >> 8) & 0xFF) << 1)
-    );
-    _delay_loop_2(150);
-    mySerial.write(
-      0xFC00 | ((leftSpeed & 0xFF) << 1),
-      0xFC00 | ((-rightSpeed & 0xFF) << 1)
-    );
-    _delay_loop_2(150);
-    mySerial.write(
-      0xFC00 | (((leftSpeed >> 8) & 0xFF) << 1),
-      0xFC00 | (((-rightSpeed >> 8) & 0xFF) << 1)
-    );
-    _delay_loop_2(150);
-    mySerial.write(endSignal, endSignal);
-    _delay_loop_2(150);
+    uint16_t startSignal = 0xFC00 | (256 << 1);
+    uint16_t endSignal = 0xFC00 | (85 << 1);
+    if (leftSpeed == 0 && rightSpeed == 0) {
+      endSignal = 0xFC00 | (170 << 1);
+    }
+
+    for(int i=0; i<framesInMessage; i++){
+      switch (currentFrame) {
+        case 1:
+          mySerial.write(startSignal, startSignal);
+          break;
+        case 2:
+          mySerial.write(
+            0xFC00 | ((leftSpeed & 0xFF) << 1),
+            0xFC00 | ((-rightSpeed & 0xFF) << 1)
+          );
+          break;
+        case 3:
+          mySerial.write(
+            0xFC00 | (((leftSpeed >> 8) & 0xFF) << 1),
+            0xFC00 | (((-rightSpeed >> 8) & 0xFF) << 1)
+          );
+          break;
+        case 4:
+          mySerial.write(
+            0xFC00 | ((leftSpeed & 0xFF) << 1),
+            0xFC00 | ((-rightSpeed & 0xFF) << 1)
+          );
+          break;
+        case 5:
+          mySerial.write(
+            0xFC00 | (((leftSpeed >> 8) & 0xFF) << 1),
+            0xFC00 | (((-rightSpeed >> 8) & 0xFF) << 1)
+          );
+          break;
+        case 6:
+          mySerial.write(endSignal, endSignal);
+          break;
+      }
+      currentFrame++;
+      if (currentFrame > 6) currentFrame = 1;
+      //_delay_loop_2(150); // delays between frames
+      // we need delay here to let interruptions happen
+    }
   }
 }
 
 /***
- * A protothread helper function calls writeCurrentSpeed after 'interval' microseconds passed
+ * A protothread helper function.
+ * Calls writeCurrentSpeed after 'interval' microseconds passed.
  */
 static int ptTransmisson(struct pt *pt, unsigned long interval) {
   static unsigned long timestamp = 0;
@@ -77,7 +125,7 @@ static int ptTransmisson(struct pt *pt, unsigned long interval) {
 
 /***
  * Reads control from serial port.
- * Development and debugging purpose only. 
+ * Development and debugging purpose only.
  */
 void listenSerialControl() {
   if (Serial.available() > 0) {
@@ -98,112 +146,19 @@ void listenSerialControl() {
 }
 
 /***
- * Checks car control panel button status and calculates the speed of the wheels.
- */
-void listenButtonControl() {
-  static unsigned long timestamp = 0;
-  unsigned int currentAcceleration;
-  unsigned int elapsedTime;
-  int maxContextualSpeed;
-  int leftForwardState;
-  int leftBackwardState;
-  int rightForwardState;
-  int rightBackwardState;
-  leftForwardState   = digitalRead(leftForwardButton);
-  leftBackwardState  = digitalRead(leftBackwardButton);
-  rightForwardState  = digitalRead(rightForwardButton);
-  rightBackwardState = digitalRead(rightBackwardButton);
-  if (timestamp > millis() || timestamp == 0) {
-    timestamp = millis();
-  }
-  elapsedTime = millis() - timestamp;
-  currentAcceleration = elapsedTime * acceleration / 1000;
-  elapsedTime = 1000 * currentAcceleration / acceleration; // elapsed and counted
-  timestamp += elapsedTime; // increment only with the counted value 
-  if (currentAcceleration > 0) {
-    if (leftForwardState == LOW && rightForwardState == LOW ) {
-      maxContextualSpeed = maxSpeed; // straight forward
-    } else {
-      maxContextualSpeed = maxManeuveringSpeed; // turn or backward
-    }
-    if (leftForwardState == LOW) {
-      if (leftSpeed < maxContextualSpeed) { // accelerate till contextual max speed
-        leftSpeed += currentAcceleration;
-        if (leftSpeed > maxContextualSpeed) leftSpeed = maxContextualSpeed;
-      } else if (leftSpeed > maxContextualSpeed) { // slow down to contextual max speed
-        leftSpeed -= currentAcceleration;
-        if (leftSpeed < maxContextualSpeed) leftSpeed = maxContextualSpeed;        
-      }
-    } else if (leftBackwardState == LOW) { // backward accelerate till contextual max speed
-      if (-leftSpeed < maxContextualSpeed) {
-        leftSpeed -= currentAcceleration;
-        if (-leftSpeed > maxContextualSpeed) leftSpeed = -maxContextualSpeed;
-      }
-    } else if (leftSpeed > 0) { // slow down to 0
-        leftSpeed -= currentAcceleration;
-        if (leftSpeed < 0) leftSpeed = 0;   
-    } else if (leftSpeed < 0) { // slow down to 0
-        leftSpeed += currentAcceleration;
-        if (leftSpeed > 0) leftSpeed = 0;   
-    }
-    if (rightForwardState == LOW) {
-      if (rightSpeed < maxContextualSpeed) { // accelerate till contextual max speed
-        rightSpeed += currentAcceleration;
-        if (rightSpeed > maxContextualSpeed) rightSpeed = maxContextualSpeed;
-      } else if (rightSpeed > maxContextualSpeed) { // slow down to contextual max speed
-        rightSpeed -= currentAcceleration;
-        if (rightSpeed < maxContextualSpeed) rightSpeed = maxContextualSpeed;        
-      }
-    } else if (rightBackwardState == LOW) { // backward accelerate till contextual max speed
-      if (-rightSpeed < maxContextualSpeed) {
-        rightSpeed -= currentAcceleration;
-        if (-rightSpeed > maxContextualSpeed) rightSpeed = -maxContextualSpeed;
-      }
-    } else if (rightSpeed > 0) { // slow down to 0
-        rightSpeed -= currentAcceleration;
-        if (rightSpeed < 0) rightSpeed = 0;   
-    } else if (rightSpeed < 0) { // slow down to 0
-        rightSpeed += currentAcceleration;
-        if (rightSpeed > 0) rightSpeed = 0;   
-    }
-  }
-}
-
-/***
- * Applies safety speed check and trigger the hijack switch.
- */
-int speedCheck() {
-  static signed int lastLeftSpeed = 0; // left wheel speed
-  static signed int lastRightSpeed = 0; // right wheel speed
-  if (leftSpeed != lastLeftSpeed || rightSpeed != lastRightSpeed) {
-    lastLeftSpeed = leftSpeed;
-    lastRightSpeed = rightSpeed;
-  }
-  // since it is a vehicle controller better to double check the speed.
-  if (abs(leftSpeed) > maxSpeed || abs(rightSpeed) > maxSpeed) {
-    Serial.println("Speed limit exceeded. Emergency break activated.");
-    leftSpeed = 0;
-    rightSpeed = 0;
-  }
-  if (digitalRead(inputSelector) == LOW && (leftSpeed || rightSpeed)) {
-    digitalWrite(inputSelector, HIGH);
-  }
-}
-
-/***
- * Selects control and apply safety speed check.
+ * Selects control.
  */
 void listenControl() {
   if(control == bySerial){
     listenSerialControl();
-  } else if (control == byButton) {
-    listenButtonControl();
+  } else if (control == byI2C) {
+    // tbd
   }
-  speedCheck(); // hijack switch, safety check
 }
 
 /***
- * A protothread helper function calls listenControl after 'interval' microseconds passed
+ * A protothread helper function.
+ * Calls listenControl after 'interval' microseconds passed.
  */
 static int ptListenControl(struct pt *pt, unsigned long interval) {
   static unsigned long timestamp = 0;
@@ -217,22 +172,121 @@ static int ptListenControl(struct pt *pt, unsigned long interval) {
   PT_END(pt);
 }
 
-void setup() {
-  pinMode(inputSelector, OUTPUT);
-  pinMode(leftForwardButton, INPUT_PULLUP);
-  pinMode(leftBackwardButton, INPUT_PULLUP);
-  pinMode(rightForwardButton, INPUT_PULLUP);
-  pinMode(rightBackwardButton, INPUT_PULLUP);
-  digitalWrite(inputSelector, LOW); // by start no hijack
-  mySerial.begin(26315, 12); // hover board UART baud-rate and frame size 
-  Serial.begin(115200); // opens serial port, sets data rate to 9600 bps
-  Serial.println("Hello CarOne");
+/***
+  * Calculates last 4 averages and sets to leftCyclePerSec and rightCyclePerSec
+  */
+void calculateIncInPeriod() {
+  static unsigned long timestamp = 0;
+  static unsigned int chpsL1 = 0, chpsL2 = 0, chpsL3 = 0, chpsL4 = 0;
+  static unsigned int chpsR1 = 0, chpsR2 = 0, chpsR3 = 0, chpsR4 = 0;
+  unsigned long timeElipsed = 0;
+  if (timestamp) {
+    chpsL4 = chpsL3;
+    chpsL3 = chpsL2;
+    chpsL2 = chpsL1;
+    timeElipsed = millis() - timestamp;
+    chpsL1 =  (1000 * (long)leftIncrements) / (long)timeElipsed;
+    leftCyclePerSec = (chpsL1 + chpsL2 + chpsL3 + chpsL4) / 4;
+  }
+  leftIncrements = 0;
+  timestamp = millis();
 }
 
-void loop() {
-  //schedule the two protothreads by calling them infinitely
-  ptTransmisson(&ptTx, 3100); // 2900ms frame transmission and 300ms in between 
-                              // transmissions, just like gyro sensor does
-  ptListenControl(&ptLC, 50000); // delay 50 milliseconds between control samples
+/***
+ * A protothread helper function.
+ * Calls calculateIncInPeriod after 'interval' microseconds passed.
+ */
+static int ptCalculateIncInPeriod(struct pt *pt, unsigned long interval) {
+  static unsigned long timestamp = 0;
+  if (timestamp > micros()) timestamp = micros();
+  PT_BEGIN(pt);
+  while(1) {
+    PT_WAIT_UNTIL(pt, micros() - timestamp > interval );
+    timestamp = micros();
+    calculateIncInPeriod();
+  }
+  PT_END(pt);
+}
+
+/***
+  * Increments changes on left hall effect sensor and detects direction.
+  */
+void hallLeftEnc() {
+  static unsigned long timestamp = 0;
+  debugPulse(debugPin, 1);
+  bool hallLeftYellowState = digitalRead(hallLeftYellowPin);
+  if (!hallLeftYellowState && (timestamp+500) < micros()) {
+    if (leftCyclePerSec < 15) //only detect direction under one rotation/s
+      leftClockwise = digitalRead(hallLeftBluePin);
+    leftIncrements++;
+    timestamp = micros();
+  }
+}
+
+// to be implemented
+void hallRightEnc() {
+}
+
+void debugInfo() {
+  Serial.print("LCPS: ");
+  Serial.print(leftCyclePerSec);
+  Serial.print("; RCPS: ");
+  Serial.println("TBI");
+  //Serial.println(millis());
+}
+
+/***
+ * A protothread helper function.
+ * Calls debugInfo after 'interval' microseconds passed.
+ */
+static int ptDebugInfo(struct pt *pt, unsigned long interval) {
+  static unsigned long timestamp = 0;
+  if (timestamp > micros()) timestamp = micros();
+  PT_BEGIN(pt);
+  while(1) {
+    PT_WAIT_UNTIL(pt, micros() - timestamp > interval );
+    timestamp = micros();
+    debugInfo();
+  }
+  PT_END(pt);
+}
+
+void setup() {
+  PT_INIT(&ptTx);
+  PT_INIT(&pIncr);
+  PT_INIT(&ptLC);
+  PT_INIT(&ptDebug);
+
+  pinMode(inputSelector, OUTPUT);
+  pinMode(debugPin, OUTPUT);
+  pinMode(hallLeftYellowPin, INPUT);
+  pinMode(hallLeftBluePin, INPUT);
+  pinMode(hallRightYellowPin, INPUT);
+  pinMode(hallRightBluePin, INPUT);
+  digitalWrite(inputSelector, LOW); // by start no hijack
+
+  attachInterrupt(digitalPinToInterrupt(hallLeftYellowPin),hallLeftEnc,FALLING);
+  attachInterrupt(digitalPinToInterrupt(hallRightYellowPin),hallRightEnc,FALLING);
+
+  mySerial.begin(26275, 13); // hover board UART baud-rate and frame size
+  if (_SERIAL) {
+    Serial.begin(_SERIAL); // opens serial port, sets data rate to 9600 bps
+    Serial.println("CarOne");
+  }
+
+  // TODO: need to be controlled from outside
+  digitalWrite(inputSelector, HIGH);
+}
+
+void loop() { //schedule the protothreads by calling them infinitely
+  // 2900ms frame transmission and 300ms in between
+  // transmissions, just like gyro sensor does
+  ptTransmisson(&ptTx, 5000);
+  // calculate increments in every 1/4 sec
+  ptCalculateIncInPeriod(&pIncr, 250000);
+  // delay 50 milliseconds between control samples
+  ptListenControl(&ptLC, 50000);
+  // delay 1s between debug info dump
+  if (_SERIAL && _DEBUG) ptDebugInfo(&ptDebug, 2000000);
 }
 
