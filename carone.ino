2cb1465a30a461505c0bb387317aca3a475165c4
oliver nadj <oliver.nadj@lazada.com>
2018-07-25 19:24:05 +0800

    Removed protothread library and re-purpose the code to work as an etension board

--- carone.ino
+++ carone.ino
@@ -1,38 +1,39 @@
 #include <ParaSerialWrite.h> // ParaSerialWrite library for parallel UART transmission
-#include <pt.h>              // protothread library
 
 #define _DEBUG 1
 #define _SERIAL 9600 // bandwidth or 0 means turned off
 
-const int debugPin            = 6;
-const int inputSelector       = 7; // hijack gyro sensor communication
-const int hallLeftYellowPin   = 2;
-const int hallLeftBluePin     = 4;
-const int hallRightYellowPin   = 3;
-const int hallRightBluePin     = 5;
+const int debugPin4            = 13; // debug during development
+const int debugPin3            = 12; // debug during development
+const int debugPin2            = 11; // debug during development
+const int debugPin1            = 10; // debug during development
+const int inputSelector        =  7; // hijack gyro sensor communication
+const int hallRightBluePin     =  5; // hall effect direction check
+const int hallLeftBluePin      =  4; // hall effect direction check
+const int hallRightYellowPin   =  3; // hall effect triggering
+const int hallLeftYellowPin    =  2; // hall effect triggering
 
-ParaSerialWrite mySerial(2); // register 2 lower ports of PORTB for parallel UART transmission
-signed int leftSpeed = 0; // left wheel speed
-signed int rightSpeed = 0; // right wheel speed
+signed int leftAcceleration = 111; // left wheel speed
+signed int rightAcceleration = 111; // right wheel speed
 bool leftClockwise = false;
 bool rightClockwise = false;
+
 
 unsigned long lastLeftIncrCalculated = 0;
 unsigned int leftIncrements = 0;
 unsigned long leftCyclePerSec = 0;
+unsigned long lastRightIncrCalculated = 0;
+unsigned int rightIncrements = 0;
+unsigned long rightCyclePerSec = 0;
 
+char shifter = 'D'; // [P]ark or [D]rive
 
-const int maxSpeed = 350; // absolute maximum speed
-const int maxManeuveringSpeed = 150; // maximum maneuvering speed
-const int acceleration = 100; // x unit acceleration / second^2
+ParaSerialWrite mySerial(2); // register 2 lower ports of PORTB for parallel UART transmission
 const int framesInMessage = 10; // vary by models. how many frames transfer before ~500us pause
 
 enum controlOptions { bySerial, byI2C }; // control by serial input or pull up buttons
 int control = bySerial; // control set to button
 char c = 'x'; //last received input by serial. x = neutral
-
-static struct pt ptTx, ptLC, pIncr, ptDebug; // each protothread needs one of these
-
 
 //
 // Debugging
@@ -55,72 +56,47 @@ void debugPulse(int pin, uint16_t count)
  * same frame size and baud-rate
  */
 void writeCurrentSpeed() {
-  if (digitalRead(inputSelector) == LOW) {
-    mySerial.write(0, 0);
+  if (digitalRead(inputSelector) == HIGH) {
+    mySerial.write(~0, ~0);
   } else {
-    static unsigned int currentFrame = 1;
     //0xC00 2 stop bits and <<1 for the start
-    uint16_t startSignal = 0xFC00 | (256 << 1);
-    uint16_t endSignal = 0xFC00 | (85 << 1);
-    if (leftSpeed == 0 && rightSpeed == 0) {
-      endSignal = 0xFC00 | (170 << 1);
-    }
+    const uint16_t startSignal = 0xFC00 | (256 << 1);
+    const uint16_t driveEndSignal = 0xFC00 | (85 << 1);
+    const uint16_t parkEndSignal = 0xFC00 | (170 << 1);
 
-    for(int i=0; i<framesInMessage; i++){
-      switch (currentFrame) {
-        case 1:
-          mySerial.write(startSignal, startSignal);
-          break;
-        case 2:
-          mySerial.write(
-            0xFC00 | ((leftSpeed & 0xFF) << 1),
-            0xFC00 | ((-rightSpeed & 0xFF) << 1)
-          );
-          break;
-        case 3:
-          mySerial.write(
-            0xFC00 | (((leftSpeed >> 8) & 0xFF) << 1),
-            0xFC00 | (((-rightSpeed >> 8) & 0xFF) << 1)
-          );
-          break;
-        case 4:
-          mySerial.write(
-            0xFC00 | ((leftSpeed & 0xFF) << 1),
-            0xFC00 | ((-rightSpeed & 0xFF) << 1)
-          );
-          break;
-        case 5:
-          mySerial.write(
-            0xFC00 | (((leftSpeed >> 8) & 0xFF) << 1),
-            0xFC00 | (((-rightSpeed >> 8) & 0xFF) << 1)
-          );
-          break;
-        case 6:
-          mySerial.write(endSignal, endSignal);
-          break;
-      }
-      currentFrame++;
-      if (currentFrame > 6) currentFrame = 1;
-      //_delay_loop_2(150); // delays between frames
-      // we need delay here to let interruptions happen
+    static unsigned int currentFrame = 1;
+    static uint16_t endSignal = parkEndSignal;
+    static uint16_t leftAccelerationLowFrame;
+    static uint16_t leftAccelerationHighFrame;
+    static uint16_t rightAccelerationLowFrame;
+    static uint16_t rightAccelerationHighFrame;
+
+    switch (currentFrame) {
+      case 1:
+        mySerial.write(~startSignal, ~startSignal);
+        leftAccelerationLowFrame = 0xFC00 | ((leftAcceleration & 0xFF) << 1);
+        leftAccelerationHighFrame = 0xFC00 | (((leftAcceleration >> 8) & 0xFF) << 1);
+        rightAccelerationLowFrame = 0xFC00 | ((-rightAcceleration & 0xFF) << 1);
+        rightAccelerationHighFrame = 0xFC00 | (((-rightAcceleration >> 8) & 0xFF) << 1);
+        endSignal = (shifter == 'D') ? driveEndSignal : parkEndSignal ;
+        currentFrame++;
+        break;
+      case 2:
+      case 4:
+        mySerial.write(~leftAccelerationLowFrame, ~rightAccelerationLowFrame);
+        currentFrame++;
+        break;
+      case 3:
+      case 5:
+        mySerial.write(~leftAccelerationHighFrame, ~rightAccelerationHighFrame);
+        currentFrame++;
+        break;
+      case 6:
+        mySerial.write(~endSignal, ~endSignal);
+        currentFrame = 1;
+        break;
     }
   }
-}
-
-/***
- * A protothread helper function.
- * Calls writeCurrentSpeed after 'interval' microseconds passed.
- */
-static int ptTransmisson(struct pt *pt, unsigned long interval) {
-  static unsigned long timestamp = 0;
-  if (timestamp > micros()) timestamp = micros();
-  PT_BEGIN(pt);
-  while(1) {
-    PT_WAIT_UNTIL(pt, micros() - timestamp > interval );
-    timestamp = micros();
-    writeCurrentSpeed();
-  }
-  PT_END(pt);
 }
 
 /***
@@ -131,16 +107,16 @@ void listenSerialControl() {
   if (Serial.available() > 0) {
     c=Serial.read();
     if(c == ' ') {
-      leftSpeed=0;
-      rightSpeed=0;
+      leftAcceleration=0;
+      rightAcceleration=0;
     } else if(c == 'q') {
-      leftSpeed += 50;
+      leftAcceleration += 50;
     } else if(c == 'a') {
-      leftSpeed -= 50;
+      leftAcceleration -= 50;
     } else if(c == 'w') {
-      rightSpeed += 50;
+      rightAcceleration += 50;
     }  else if(c == 's') {
-      rightSpeed -= 50;
+      rightAcceleration -= 50;
     }
   }
 }
@@ -154,22 +130,6 @@ void listenControl() {
   } else if (control == byI2C) {
     // tbd
   }
-}
-
-/***
- * A protothread helper function.
- * Calls listenControl after 'interval' microseconds passed.
- */
-static int ptListenControl(struct pt *pt, unsigned long interval) {
-  static unsigned long timestamp = 0;
-  if (timestamp > micros()) timestamp = micros();
-  PT_BEGIN(pt);
-  while(1) {
-    PT_WAIT_UNTIL(pt, micros() - timestamp > interval );
-    timestamp = micros();
-    listenControl();
-  }
-  PT_END(pt);
 }
 
 /***
@@ -187,25 +147,16 @@ void calculateIncInPeriod() {
     timeElipsed = millis() - timestamp;
     chpsL1 =  (1000 * (long)leftIncrements) / (long)timeElipsed;
     leftCyclePerSec = (chpsL1 + chpsL2 + chpsL3 + chpsL4) / 4;
+    chpsR4 = chpsR3;
+    chpsR3 = chpsR2;
+    chpsR2 = chpsR1;
+    timeElipsed = millis() - timestamp;
+    chpsR1 =  (1000 * (long)rightIncrements) / (long)timeElipsed;
+    rightCyclePerSec = (chpsR1 + chpsR2 + chpsR3 + chpsR4) / 4;
   }
   leftIncrements = 0;
+  rightIncrements = 0;
   timestamp = millis();
-}
-
-/***
- * A protothread helper function.
- * Calls calculateIncInPeriod after 'interval' microseconds passed.
- */
-static int ptCalculateIncInPeriod(struct pt *pt, unsigned long interval) {
-  static unsigned long timestamp = 0;
-  if (timestamp > micros()) timestamp = micros();
-  PT_BEGIN(pt);
-  while(1) {
-    PT_WAIT_UNTIL(pt, micros() - timestamp > interval );
-    timestamp = micros();
-    calculateIncInPeriod();
-  }
-  PT_END(pt);
 }
 
 /***
@@ -213,7 +164,7 @@ static int ptCalculateIncInPeriod(struct pt *pt, unsigned long interval) {
   */
 void hallLeftEnc() {
   static unsigned long timestamp = 0;
-  debugPulse(debugPin, 1);
+  debugPulse(debugPin1, 1);
   bool hallLeftYellowState = digitalRead(hallLeftYellowPin);
   if (!hallLeftYellowState && (timestamp+500) < micros()) {
     if (leftCyclePerSec < 15) //only detect direction under one rotation/s
@@ -223,70 +174,96 @@ void hallLeftEnc() {
   }
 }
 
-// to be implemented
+/***
+  * Increments changes on right hall effect sensor and detects direction.
+  */
 void hallRightEnc() {
+  static unsigned long timestamp = 0;
+  debugPulse(debugPin1, 1);
+  bool hallRightYellowState = digitalRead(hallRightYellowPin);
+  if (!hallRightYellowState && (timestamp+500) < micros()) {
+    if (rightCyclePerSec < 15) //only detect direction under one rotation/s
+      rightClockwise = digitalRead(hallRightBluePin);
+    rightIncrements++;
+    timestamp = micros();
+  }
 }
 
 void debugInfo() {
   Serial.print("LCPS: ");
   Serial.print(leftCyclePerSec);
   Serial.print("; RCPS: ");
-  Serial.println("TBI");
+  Serial.println(rightCyclePerSec);
   //Serial.println(millis());
 }
 
-/***
- * A protothread helper function.
- * Calls debugInfo after 'interval' microseconds passed.
- */
-static int ptDebugInfo(struct pt *pt, unsigned long interval) {
-  static unsigned long timestamp = 0;
-  if (timestamp > micros()) timestamp = micros();
-  PT_BEGIN(pt);
-  while(1) {
-    PT_WAIT_UNTIL(pt, micros() - timestamp > interval );
-    timestamp = micros();
-    debugInfo();
-  }
-  PT_END(pt);
-}
-
 void setup() {
-  PT_INIT(&ptTx);
-  PT_INIT(&pIncr);
-  PT_INIT(&ptLC);
-  PT_INIT(&ptDebug);
 
-  pinMode(inputSelector, OUTPUT);
-  pinMode(debugPin, OUTPUT);
+  pinMode(debugPin1, OUTPUT);
+  pinMode(debugPin2, OUTPUT);
+  pinMode(debugPin3, OUTPUT);
+  pinMode(debugPin4, OUTPUT);
   pinMode(hallLeftYellowPin, INPUT);
   pinMode(hallLeftBluePin, INPUT);
   pinMode(hallRightYellowPin, INPUT);
   pinMode(hallRightBluePin, INPUT);
-  digitalWrite(inputSelector, LOW); // by start no hijack
+  pinMode(inputSelector, OUTPUT);
+  digitalWrite(inputSelector, HIGH); // by start no hijack
 
   attachInterrupt(digitalPinToInterrupt(hallLeftYellowPin),hallLeftEnc,FALLING);
   attachInterrupt(digitalPinToInterrupt(hallRightYellowPin),hallRightEnc,FALLING);
 
-  mySerial.begin(26275, 13); // hover board UART baud-rate and frame size
+  mySerial.begin(26275, 11); // hover board UART baud-rate and frame size
   if (_SERIAL) {
     Serial.begin(_SERIAL); // opens serial port, sets data rate to 9600 bps
     Serial.println("CarOne");
   }
 
   // TODO: need to be controlled from outside
-  digitalWrite(inputSelector, HIGH);
+  // digitalWrite(inputSelector, LOW);
 }
 
-void loop() { //schedule the protothreads by calling them infinitely
-  // 2900ms frame transmission and 300ms in between
+void loop() {
+  static unsigned long pmTransmisson =  0;
+  static unsigned long pmCalculateIncInPeriod =  0;
+  static unsigned long pmListenControl =  0;
+  static unsigned long pmDebugInfo =  0;
+  static unsigned int transmissionPeriod = 450;
+  static unsigned int transmissionCounter = 0;
+  unsigned long currentMicros = micros();
+
+  // Short 450 us frame cycles (time between 2 start bit) happen 10/9 times
+  // Long 875 us cycles (time between 2 start bit) happen 10/1 times
   // transmissions, just like gyro sensor does
-  ptTransmisson(&ptTx, 5000);
+  if (currentMicros - pmTransmisson >= transmissionPeriod) {
+    pmTransmisson = currentMicros;
+    writeCurrentSpeed();
+    transmissionPeriod = (++transmissionCounter % framesInMessage == 0)
+      ? 875 : 450;
+    currentMicros = micros();
+  }
+
   // calculate increments in every 1/4 sec
-  ptCalculateIncInPeriod(&pIncr, 250000);
+  if (currentMicros - pmCalculateIncInPeriod >= 250000) {
+    pmCalculateIncInPeriod = currentMicros;
+    calculateIncInPeriod();
+    currentMicros = micros();
+  }
+
   // delay 50 milliseconds between control samples
-  ptListenControl(&ptLC, 50000);
+  if (currentMicros - pmListenControl >= 50000) {
+    pmListenControl = currentMicros;
+    listenControl();
+    currentMicros = micros();
+  }
+
   // delay 1s between debug info dump
-  if (_SERIAL && _DEBUG) ptDebugInfo(&ptDebug, 2000000);
+  if (_SERIAL && _DEBUG) {
+    if (currentMicros - pmDebugInfo >= 2000000) {
+      pmDebugInfo = currentMicros;
+      debugInfo();
+      currentMicros = micros();
+    }
+  }
 }
 
