64ef3ef4356166b6542cd081e140de9258e35656
oliver nadj <oliver.nadj@lazada.com>
2018-08-04 23:00:00 +0700

    Proof of cencept for software error correction of hall efect sensor. Plus code re-org

--- carone.ino
+++ carone.ino
@@ -1,26 +1,38 @@
 #include <SoftSerialParallelWrite.h> // SoftSerialParallelWrite library for parallel UART transmission
 
 #define _DEBUG 1
-#define _SERIAL 9600 // bandwidth or 0 means turned off
+#define _GYRO_SERIAL 26275
+#define _GYRO_FRAMES 10 // number of frames in one batch
+// Example:
+//   Short 450 us frame cycles (time between 2 start bit) happen 10/9 times
+//   Long 875 us cycles (time between 2 start bit) happen 10/1 times
+#define _GYRO_FRAME_LENGTH 450 // frame length
+#define _GYRO_FRAMES_LENGTH 875 // frame length + delay between batch
 
-const int debugPin2            = A5; // debug during development
-const int debugPin1            = A4; // debug during development
-const int inputSelector        =  7; // hijack gyro sensor communication
-const int hallRightBluePin     =  5; // hall effect direction check
-const int hallLeftBluePin      =  4; // hall effect direction check
-const int hallRightYellowPin   =  3; // hall effect triggering
-const int hallLeftYellowPin    =  2; // hall effect triggering
-const int maxAcceleration      = 2900; // measured value from gyro board at ~30 degree
+const int controlSerial       =  9600; // bandwidth or 0 means turned off
+const int debugPin2           =    A5; // debug during development
+const int debugPin1           =    A4; // debug during development
+const int inputSelector       =     7; // hijack gyro sensor communication
+const int rightHallBluePin    =     5; // hall effect direction check
+const int leftHallBluePin     =     4; // hall effect direction check
+const int rightHallYellowPin  =     3; // hall effect triggering
+const int leftHallYellowPin   =     2; // hall effect triggering
+const int maxAcceleration     =  2900; // measured value from gyro board at ~30 degree
 
-volatile unsigned int leftIncrements = 0;
-volatile unsigned long leftCyclePerSec = 0;
-volatile unsigned int rightIncrements = 0;
-volatile unsigned long rightCyclePerSec = 0;
-volatile bool leftClockwise = false;
-volatile bool rightClockwise = false;
+volatile unsigned long leftHallInterrupted = 0; //0 means not otherwise micro time stamp
+volatile unsigned long rightHallInterrupted = 0; //0 means not otherwise micro time stamp
+unsigned int leftIncrements = 0;
+unsigned long leftCyclePerSec = 0;
+unsigned int rightIncrements = 0;
+unsigned long rightCyclePerSec = 0;
+// although ccw status logically is a boolean, we need to avoid hall check error
+// with software solution. Increasing of the status on each sampling absorb the
+// occasional sampling errors.
+signed int leftClockwise = 0; // ccw: -3, -2, -1; cw: 0, 1, 2 
+signed int rightClockwise = 0; 
+
 
 SoftSerialParallelWrite mySerial(2); // register 2 lower ports of PORTB for parallel UART transmission
-const int framesInMessage = 10; // vary by models. how many frames transfer before ~500us pause
 
 enum controlOptions { bySerial, byI2C }; // control by serial input or pull up buttons
 int control = bySerial; // control set to button
@@ -34,7 +46,8 @@ signed int rightAcceleration = 0;
 //
 // This function generates a brief pulse
 // for debugging or measuring on an oscilloscope.
-void debugPulse(int pin, uint16_t count)
+#if _DEBUG
+inline void debugPulse(uint8_t pin, uint16_t count)
 {
   bool pinStatus = digitalRead(pin);
   while (count--)
@@ -43,11 +56,17 @@ void debugPulse(int pin, uint16_t count)
     digitalWrite(pin, pinStatus);
   }
 }
+#else
+inline void DebugPulse(uint8_t, uint16_t) {}
+#endif
 
 /***
  * Transmits imitated UART signal, just like gyro sensor.
  * Uses SoftSerialParallelWrite library what can do multiple UART transmission with
  * same frame size and baud-rate
+ *
+ * ATTENTION: the logical high and low has been inverted (to be align with NOT
+ * gate on logic level shift from 5v to 3.3v) 
  */
 void writeCurrentSpeed() {
   if (digitalRead(inputSelector) == HIGH) {
@@ -58,7 +77,7 @@ void writeCurrentSpeed() {
     const uint16_t driveEndSignal = 0xFC00 | (85 << 1);
     const uint16_t parkEndSignal = 0xFC00 | (170 << 1);
 
-    static unsigned int currentFrame = 1;
+    static unsigned int currentFrame = 6;
     static uint16_t endSignal = parkEndSignal;
     static uint16_t leftAccelerationLowFrame;
     static uint16_t leftAccelerationHighFrame;
@@ -173,31 +192,51 @@ void calculateIncInPeriod() {
 /***
   * Increments changes on left hall effect sensor and detects direction.
   */
-void hallLeftEnc() {
-  static unsigned long timestamp = 0;
+void leftHallInc() {
   debugPulse(debugPin1, 1);
-  bool hallLeftYellowState = digitalRead(hallLeftYellowPin);
-  if (!hallLeftYellowState && (timestamp+500) < micros()) {
-    if (leftCyclePerSec < 15) //only detect direction under one wheel rotation/s
-      leftClockwise = digitalRead(hallLeftBluePin);
+  bool leftHallYellowState = digitalRead(leftHallYellowPin);
+  if (!leftHallYellowState) {
+    if (leftCyclePerSec < 15) {//only detect direction under one rotation/s
+      if (digitalRead(leftHallBluePin)) {
+        if (leftClockwise < 2) leftClockwise++;
+      } else {
+        if (leftClockwise > -3) leftClockwise--;        
+      }
+    }
     leftIncrements++;
-    timestamp = micros();
   }
 }
 
 /***
   * Increments changes on right hall effect sensor and detects direction.
   */
-void hallRightEnc() {
-  static unsigned long timestamp = 0;
+void rightHallInc() {
   debugPulse(debugPin1, 1);
-  bool hallRightYellowState = digitalRead(hallRightYellowPin);
-  if (!hallRightYellowState && (timestamp+500) < micros()) {
-    if (rightCyclePerSec < 15) //only detect direction under one rotation/s
-      rightClockwise = digitalRead(hallRightBluePin);
+  bool rightHallYellowState = digitalRead(rightHallYellowPin);
+  if (!rightHallYellowState) {
+    if (rightCyclePerSec < 15) {//only detect direction under one rotation/s
+      if (digitalRead(rightHallBluePin)) {
+        if (rightClockwise < 2) rightClockwise++;
+      } else {
+        if (rightClockwise > -3) rightClockwise--;        
+      }
+    }
     rightIncrements++;
-    timestamp = micros();
   }
+}
+
+/***
+  * Handles interrupt.
+  */
+void leftHallInterrupt() {
+  leftHallInterrupted = micros() + 500;
+}
+
+/***
+  * Handles interrupt.
+  */
+void rightHallInterrupt() {
+  rightHallInterrupted = micros() + 500;
 }
 
 void serialInfo() {
@@ -207,10 +246,10 @@ void serialInfo() {
   Serial.print(";");
   Serial.print(shifter);
   Serial.print(";");
-  if (!leftClockwise) Serial.print("-");
+  if (leftClockwise < 0) Serial.print("-");
   Serial.print(leftCyclePerSec);
   Serial.print(";");
-  if (!rightClockwise) Serial.print("-");
+  if (!rightClockwise < 0) Serial.print("-");
   Serial.print(rightCyclePerSec);
   Serial.print(";");
   Serial.print(leftAcceleration);
@@ -220,22 +259,19 @@ void serialInfo() {
 }
 
 void setup() {
-
   pinMode(debugPin1, OUTPUT);
   pinMode(debugPin2, OUTPUT);
-  pinMode(hallLeftYellowPin, INPUT);
-  pinMode(hallLeftBluePin, INPUT);
-  pinMode(hallRightYellowPin, INPUT);
-  pinMode(hallRightBluePin, INPUT);
+  pinMode(leftHallYellowPin, INPUT);
+  pinMode(leftHallBluePin, INPUT);
+  pinMode(rightHallYellowPin, INPUT);
+  pinMode(rightHallBluePin, INPUT);
   pinMode(inputSelector, OUTPUT);
   digitalWrite(inputSelector, HIGH); // by start no hijack
-
-  attachInterrupt(digitalPinToInterrupt(hallLeftYellowPin),hallLeftEnc,FALLING);
-  attachInterrupt(digitalPinToInterrupt(hallRightYellowPin),hallRightEnc,FALLING);
-
-  mySerial.begin(26275, 11); // hover board UART baud-rate and frame size
-  if (_SERIAL) {
-    Serial.begin(_SERIAL); // opens serial port
+  attachInterrupt(digitalPinToInterrupt(leftHallYellowPin),leftHallInterrupt,FALLING);
+  attachInterrupt(digitalPinToInterrupt(rightHallYellowPin),rightHallInterrupt,FALLING);
+  mySerial.begin(_GYRO_SERIAL, 11); // hover board UART baud-rate and frame size
+  if (controlSerial) {
+    Serial.begin(controlSerial); // opens serial port
     Serial.setTimeout(10); // 10ms should be enough to receive few chars
     Serial.println("\n         _    _\n         \\`../ |o_..__\n          (*)______(*).>\n\n");
     Serial.println("Hi there, CarOne is ready to drive.");
@@ -247,53 +283,64 @@ void setup() {
     Serial.println("\tn\t Reset acceleration");
     Serial.println("\tq\t Set left side acceleration min: -2900, max: 2900");
     Serial.println("\tw\t Set left side acceleration min: -2900, max: 2900");
-    Serial.println("\nLast Command; Hijack; Shifter; Left Cycle Per Sec; Right Cycle Per Sec; Left Acceleration; Right Acceleration;");
+    Serial.print("\nLast Command; Hijack; Shifter; Left Cycle Per Sec; ");
+    Serial.println("Right Cycle Per Sec; Left Acceleration; Right Acceleration;");
   }
-
-  // TODO: need to be controlled from outside
-  // digitalWrite(inputSelector, LOW);
 }
 
+/***
+ * The loop function is responsible for (time) even handling.
+ * The checks are ordered by importance. Maximum one hander function can be 
+ * called in each loop. E.g. serial write will not be delayed with multiple
+ * handlers in one loop.
+ */
 void loop() {
   static unsigned long pmTransmisson =  0;
   static unsigned long pmCalculateIncInPeriod =  0;
   static unsigned long pmListenControl =  0;
   static unsigned long pmSerialInfo =  0;
-  static unsigned int transmissionPeriod = 450;
+  static unsigned int transmissionLength = _GYRO_FRAME_LENGTH;
   static unsigned int transmissionCounter = 0;
   unsigned long currentMicros = micros();
 
-  // Short 450 us frame cycles (time between 2 start bit) happen 10/9 times
-  // Long 875 us cycles (time between 2 start bit) happen 10/1 times
-  // transmissions, just like gyro sensor does
-  if (currentMicros - pmTransmisson >= transmissionPeriod) {
+  // transmissions, just like gyro sensor does 
+  if (currentMicros - pmTransmisson >= transmissionLength) {
     pmTransmisson = currentMicros;
     writeCurrentSpeed();
-    transmissionPeriod = (++transmissionCounter % framesInMessage == 0)
-      ? 875 : 450;
-    currentMicros = micros();
+    transmissionLength = (++transmissionCounter % _GYRO_FRAMES == 0)
+      ? _GYRO_FRAMES_LENGTH : _GYRO_FRAME_LENGTH;
+    return;
   }
 
-  // calculate increments in every 1/4 sec
+  if (leftHallInterrupted && leftHallInterrupted < currentMicros) {
+    leftHallInc();
+    leftHallInterrupted = 0;
+    return;
+  }
+
+  if (rightHallInterrupted && rightHallInterrupted < currentMicros) {
+    rightHallInc();
+    rightHallInterrupted = 0;
+    return;
+  }
+
   if (currentMicros - pmCalculateIncInPeriod >= 250000) {
     pmCalculateIncInPeriod = currentMicros;
     calculateIncInPeriod();
-    currentMicros = micros();
+    return;
   }
 
-  // delay 50 milliseconds between control samples
   if (currentMicros - pmListenControl >= 50000) {
     pmListenControl = currentMicros;
     listenControl();
-    currentMicros = micros();
+    return;
   }
 
-  // delay 1s between debug info dump
-  if (_SERIAL && _DEBUG) {
+  if (controlSerial && _DEBUG) {
     if (currentMicros - pmSerialInfo >= 2000000) {
       pmSerialInfo = currentMicros;
       serialInfo();
-      currentMicros = micros();
+      return;
     }
   }
 }
